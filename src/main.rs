402a6e0ada5c14ce3fce82983c0f7843d24009d6
adishgarg <gargadi456@gmail.com>
2024-07-04 11:58:03 +0530

    array stuff

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,44 @@ use std::io;
 //     let _x = 6;
 // }
 
-fn main(){
-    let x: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let size = x.len();
-    println!("Size of x: {}", size);
-    println!("value of x is: {}", &x);
+// fn main(){
+//     let x: [i32; 10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//     let size = x.len();
+//     println!("Size of x: {}", size);
+//     println!("value of x is: {}", &x);
+// }
+
+
+fn main() {
+    let mut x = [0; 10]; 
+    for i in 0..10 {
+        let mut input_string = String::new();
+        io::stdin().read_line(&mut input_string)
+            .expect("Failed to read line");
+        x[i] = input_string.trim().parse()
+            .expect("Please type a number");
+    }
+    swapadjacentelements(&x::i32);
+    for i in 0..10 {
+        println!("{}", x[i]);
+    }
 }
+
+fn swapadjacentelements(arr: &mut [i32; 10]) {
+    for i in (0..arr.len()).step_by(2) {
+        if i + 1 < arr.len() {
+            arr.swap(i, i + 1);
+        }
+    }
+}
+
+// fn maxinarray(arr: &[i32; 10]) -> i32 {
+//     let mut max = arr[0];
+//     for &val in arr.iter() {
+//         if val > max {
+//             max = val;
+//         }
+//     }
+//     max
+// }
+
